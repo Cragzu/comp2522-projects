7ced5b9c68ee6c951930292ef937cff0657f3d5b
Cragzu <cragzu@gmail.com>
2020-01-16 16:14:28 -0800

    Began setup of Hare move method

--- src/ca/bcit/comp2522/labs/lab01/Hare.java
+++ src/ca/bcit/comp2522/labs/lab01/Hare.java
@@ -1,4 +1,18 @@
 package ca.bcit.comp2522.labs.lab01;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Hare {
+    int position = 0;
+
+    public void move() {
+        final Random generator = new Random();
+        final int answer = generator.nextInt(3) + 1;
+        System.out.println(answer);
+    }
+
+    public static void main(String[] args) {
+        Hare myHare = new Hare();
+        System.out.println(myHare.position);
+    }
 }
