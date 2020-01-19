35730ed400b086c03be4c884d89a604e557bc9a6
Cragzu <cragzu@gmail.com>
2020-01-19 11:49:29 -0800

    Created constructor, instance variables, string capitalization method

--- src/ca/bcit/comp2522/assignments/a1/Guppy.java
+++ src/ca/bcit/comp2522/assignments/a1/Guppy.java
@@ -16,4 +16,49 @@ public class Guppy {
     final static double MINIMUM_HEALTH_COEFFICIENT = 0.0;
     final static double MAXIMUM_HEALTH_COEFFICIENT = 1.0;
 
+    private String genus;
+    private String species;
+    private int ageInWeeks;
+    private boolean isFemale;
+    private int generationNumber;
+    private boolean isAlive;
+    private double healthCoefficient;
+    private int identificationNumber;
+    private static int numberOfGuppiesBorn = 0;
+
+    /**
+     * Constructor.
+     */
+    public Guppy() {
+        numberOfGuppiesBorn += 1;
+
+        ageInWeeks = 0;
+        generationNumber = 0;
+        genus = DEFAULT_GENUS;
+        species = DEFAULT_SPECIES;
+        isFemale = true;
+        isAlive = true;
+        healthCoefficient = DEFAULT_HEALTH_COEFFICIENT;
+        identificationNumber = numberOfGuppiesBorn;
+    }
+
+    /**
+     * Converts a string to title case; the first letter capitalized and the rest lowercase.
+     *
+     * @param originalString the string to be converted.
+     * @return the capitalized string.
+     */
+    public String toTitleCase(String originalString) {
+        String newString = originalString.toLowerCase();
+        char firstLetter = newString.charAt(0);
+        newString = newString.replace(firstLetter, Character.toUpperCase(firstLetter));
+        return newString;
+    }
+
+    public static void main(String[] args) {
+        Guppy myGuppy = new Guppy();
+        String test = "hello";
+        System.out.println(myGuppy.toTitleCase(test));
+    }
+
 }
