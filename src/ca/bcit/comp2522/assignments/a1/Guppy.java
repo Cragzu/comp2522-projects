4d047a304a4b0ff16489fb40576dd44670696ba4
Cragzu <cragzu@gmail.com>
2020-01-19 11:10:00 -0800

    Created Guppy class with constants

---
+++ src/ca/bcit/comp2522/assignments/a1/Guppy.java
@@ -0,0 +1,19 @@
+package ca.bcit.comp2522.assignments.a1;
+
+/**
+ * @author Chloe Glave
+ * @version 2020
+ */
+public class Guppy {
+    /*Constants*/
+    final static int YOUNG_FISH_AGE_IN_WEEKS = 10;
+    final static int MATURE_FISH_AGE_IN_WEEKS = 30;
+    final static int MAXIMUM_AGE_IN_WEEKS = 50;
+    final static double MINIMUM_WATER_VOLUME_ML = 250.0;
+    final static String DEFAULT_GENUS = "Poecilia";
+    final static String DEFAULT_SPECIES = "reticulata";
+    final static double DEFAULT_HEALTH_COEFFICIENT = 0.5;
+    final static double MINIMUM_HEALTH_COEFFICIENT = 0.0;
+    final static double MAXIMUM_HEALTH_COEFFICIENT = 1.0;
+
+}
