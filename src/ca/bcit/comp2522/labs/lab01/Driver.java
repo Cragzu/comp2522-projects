bba820a27e167820a7757005d41c534914949fb7
Cragzu <cragzu@gmail.com>
2020-01-21 22:30:46 -0800

    Checkstyle compliance edits for Driver

--- src/ca/bcit/comp2522/labs/lab01/Driver.java
+++ src/ca/bcit/comp2522/labs/lab01/Driver.java
@@ -8,45 +8,49 @@ package ca.bcit.comp2522.labs.lab01;
  * @version 2020
  */
 public class Driver {
-    private static int NUM_RACES = 100;
-    private static int RACE_LENGTH_1 = 100;
-    private static int RACE_LENGTH_2 = 1000;
+    /**Number of races to simulate.*/
+    static final int NUM_RACES = 100;
+
+    /**Length of races to simulate in the first tournament.*/
+    static final int RACE_LENGTH_1 = 100;
+
+    /**Length of races to simulate in the second tournament.*/
+    static final int RACE_LENGTH_2 = 1000;
 
     /**
      * Simulates a series of races to determine which racer wins most often.
      *
      * @param numOfRaces the number of races to simulate.
      * @param lengthOfRace the length of each simulated race.
-     * @return a string reporting information about the races (the number of wins for each racer, overall winner).
+     * @return a string reporting information about the races
+     * (the number of wins for each racer, overall winner).
      */
     private static String simulateRaces(int numOfRaces, int lengthOfRace) {
         Race raceInstance = new Race(lengthOfRace);
         int hareWins = 0;
         int tortoiseWins = 0;
-        String output = "Starting a tournament with " + numOfRaces + " races of length " + lengthOfRace + ".\n";
+        String output = "Starting a tournament with " + numOfRaces
+                + " races of length " + lengthOfRace + ".\n";
 
-        for(int lap = 0; lap < numOfRaces; lap++) {
+        for (int lap = 0; lap < numOfRaces; lap++) {
             String roundWinner = raceInstance.simulateRace();
-            if(roundWinner.equals("Hare")) {
+            if (roundWinner.equals("Hare")) {
                 hareWins++;
-            }
-            else {
+            } else {
                 tortoiseWins++;
             }
         }
-        output += "Tortoise won " + tortoiseWins + " times, with a final position of " +
-                raceInstance.racerTortoise.getPosition() + ".\n";
-        output += "Hare won " + hareWins + " times, with a final position of " +
-                raceInstance.racerHare.getPosition() + ".\n";
+        output += "Tortoise won " + tortoiseWins + " times, with a final position of "
+                + raceInstance.racerTortoise.getPosition() + ".\n";
+        output += "Hare won " + hareWins + " times, with a final position of "
+                + raceInstance.racerHare.getPosition() + ".\n";
         output += "The last race took " + raceInstance.numOfTicks + " ticks to complete.\n";
 
         if (tortoiseWins > hareWins) {
             output += "Tortoise won overall!";
-        }
-        else if (tortoiseWins < hareWins) {
+        } else if (tortoiseWins < hareWins) {
             output += "Hare won overall!";
-        }
-        else {
+        } else {
             output += "The two won the same number of times, wow!";
         }
         output += "\n";
