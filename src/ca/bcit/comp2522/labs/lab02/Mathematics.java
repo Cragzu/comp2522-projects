890c86dcf15013753f51ec4fb1573c7e2f28d728
Cragzu <cragzu@gmail.com>
2020-01-23 15:20:47 -0800

    Bug fix edits in sumOfProducts, changes to const variables in random generator

--- src/ca/bcit/comp2522/labs/lab02/Mathematics.java
+++ src/ca/bcit/comp2522/labs/lab02/Mathematics.java
@@ -9,14 +9,23 @@ import java.util.Random;
  * @version 2020
  */
 public class Mathematics {
-    /**Value of the mathematical constant Pi.*/
+    /**
+     * Value of the mathematical constant Pi.
+     */
     static final double PI = 3.14159;
 
-    /**Ratio of foot to kilometre (1 foot = [this ratio] kilometres).*/
+    /**
+     * Ratio of foot to kilometre (1 foot = [this ratio] kilometres).
+     */
     static final double ONE_FOOT_TO_KILOMETRE_RATIO = 0.0003048;
+
+    static final int RANDOM_NUM_LOWER_BOUND = 10;
+    static final int RANDOM_NUM_UPPER_BOUND = 21;
+    static final int RANDOM_NUM_FORBIDDEN_VALUE = 15;
 
     /**
      * Calculates the area of a circle given the radius.
+     *
      * @param radius the circle's radius.
      * @return the area of the circle.
      */
@@ -26,6 +35,7 @@ public class Mathematics {
 
     /**
      * Calculates the area of a square given the length of one side.
+     *
      * @param sideLength the length of one side of the square.
      * @return the area of the square.
      */
@@ -35,7 +45,8 @@ public class Mathematics {
 
     /**
      * Adds two numbers together.
-     * @param firstNumber the first number to be added.
+     *
+     * @param firstNumber  the first number to be added.
      * @param secondNumber the second number to be added.
      * @return the sum of the two numbers.
      */
@@ -45,7 +56,8 @@ public class Mathematics {
 
     /**
      * Multiplies two numbers together.
-     * @param firstNumber the first number to be multiplied.
+     *
+     * @param firstNumber  the first number to be multiplied.
      * @param secondNumber the second number to be multiplied.
      * @return the product of the two numbers.
      */
@@ -55,7 +67,8 @@ public class Mathematics {
 
     /**
      * Subtracts one number from another.
-     * @param minuend the number to be subtracted from.
+     *
+     * @param minuend    the number to be subtracted from.
      * @param subtrahend the number to subtract.
      * @return the difference between the two numbers.
      */
@@ -67,7 +80,7 @@ public class Mathematics {
      * Divides one number by another.
      *
      * @param dividend the number to divide.
-     * @param divisor the amount of groups to divide into.
+     * @param divisor  the amount of groups to divide into.
      * @return the quotient of the two numbers.
      */
     public double divide(double dividend, double divisor) {
@@ -79,6 +92,7 @@ public class Mathematics {
 
     /**
      * Calculates the absolute value of a number.
+     *
      * @param num the number to get the absolute value of.
      * @return the absolute value of the given num.
      */
@@ -88,19 +102,22 @@ public class Mathematics {
 
     /**
      * Generates a random integer between 10 and 20, that cannot be 15.
+     *
      * @return the generated random number.
      */
     public int getRandomNumberBetweenTenAndTwentyButNotFifteen() {
         int result;
         do {
             Random generator = new Random();
-            result = generator.nextInt(20-10) + 10; /*generate random num between 0-9*/
-        } while (result == 15);
+            result = generator.nextInt(RANDOM_NUM_UPPER_BOUND - RANDOM_NUM_LOWER_BOUND)
+                    + RANDOM_NUM_LOWER_BOUND; /*generate random num between 0-9*/
+        } while (result == RANDOM_NUM_FORBIDDEN_VALUE);
         return result;
     }
 
     /**
      * Converts a distance in feet to the equivalent distance in kilometres.
+     *
      * @param distanceInFeet the initial distance in feet.
      * @return the distance in kilometres.
      */
@@ -161,14 +178,23 @@ public class Mathematics {
      * that are divisible by the second parameter.
      *
      * @param upperBound the point at which to stop adding.
-     * @param divisor the number to divide each int by; checking whether to add it to the sum.
+     * @param divisor    the number to divide each int by; checking whether to add it to the sum.
      * @return the sum of the numbers meeting the requirements.
      */
     public int sumOfProducts(int upperBound, int divisor) {
         int sum = 0;
-        for (int i = 0; i <= upperBound; i++) {
-            if (i % divisor == 0) {
-                sum += i;
+
+        if (isPositive(upperBound)) {
+            for (int positiveIterator = 0; positiveIterator <= upperBound; positiveIterator++) {
+                if (positiveIterator % divisor == 0) {
+                    sum += positiveIterator;
+                }
+            }
+        } else {
+            for (int negativeIterator = 0; negativeIterator >= upperBound; negativeIterator--) {
+                if (negativeIterator % divisor == 0) {
+                    sum += negativeIterator;
+                }
             }
         }
         return sum;
