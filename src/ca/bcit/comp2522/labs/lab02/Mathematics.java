aedd8b02360d26e943429c984d3f7e40e13678b7
Cragzu <cragzu@gmail.com>
2020-01-23 11:52:23 -0800

    Created lab02 files, set up constants

---
+++ src/ca/bcit/comp2522/labs/lab02/Mathematics.java
@@ -0,0 +1,18 @@
+package ca.bcit.comp2522.labs.lab02;
+
+/**
+ * Creates a Mathematics object with methods for various mathematical operations.
+ *
+ * @author Chloe Glave
+ * @version 2020
+ */
+public class Mathematics {
+    /**Value of the mathematical constant Pi.*/
+    static final double PI = 3.14159;
+
+    /**Ratio of foot to kilometre (1 foot = [this ratio] kilometres).*/
+    static final double ONE_FOOT_TO_KILOMETRE_RATIO = 0.0003048;
+
+
+
+}
