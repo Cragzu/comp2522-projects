81a3d94e1691bdb9bb26a2c46d94305cc44b98b6
Cragzu <cragzu@gmail.com>
2020-01-30 15:18:18 -0800

    Created plantation class

---
+++ src/ca/bcit/comp2522/labs/lab03/Plantation.java
@@ -0,0 +1,18 @@
+package ca.bcit.comp2522.labs.lab03;
+
+import java.util.ArrayList;
+
+/**
+ * Creates and manages a Plantation containing Trees.
+ *
+ * @author Chloe Glave
+ * @version 2020
+ */
+public class Plantation {
+    private ArrayList<Tree> farm = new ArrayList<>();
+
+    public int size() {
+        return farm.size();
+    }
+
+}
