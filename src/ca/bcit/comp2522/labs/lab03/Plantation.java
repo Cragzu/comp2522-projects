587577a5d0d161c5e772aa895aa5f4a790439d1c
Cragzu <cragzu@gmail.com>
2020-01-30 15:22:34 -0800

    Created add method

--- src/ca/bcit/comp2522/labs/lab03/Plantation.java
+++ src/ca/bcit/comp2522/labs/lab03/Plantation.java
@@ -11,8 +11,24 @@ import java.util.ArrayList;
 public class Plantation {
     private ArrayList<Tree> farm = new ArrayList<>();
 
+    /**
+     * Returns the current size of the farm; the number of trees there are.
+     * @return the number of trees in the farm.
+     */
     public int size() {
         return farm.size();
+    }
+
+    /**
+     * Adds a tree to the farm.
+     * @param sapling the new tree to be added.
+     */
+    public void add(Tree sapling) {
+        if (sapling == null) {
+            throw new NullPointerException("That sapling doesn't exist!");
+        } else {
+            farm.add(sapling);
+        }
     }
 
 }
