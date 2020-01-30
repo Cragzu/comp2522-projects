cf5a4b7f4b6cfefb67ece0b063658f10ccef9510
Cragzu <cragzu@gmail.com>
2020-01-30 15:29:58 -0800

    Created seed method and necessary consts

--- src/ca/bcit/comp2522/labs/lab03/Plantation.java
+++ src/ca/bcit/comp2522/labs/lab03/Plantation.java
@@ -1,6 +1,7 @@
 package ca.bcit.comp2522.labs.lab03;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Creates and manages a Plantation containing Trees.
@@ -9,6 +10,24 @@ import java.util.ArrayList;
  * @version 2020
  */
 public class Plantation {
+    /**Minimum number of trees that can be planted when the farm is seeded.*/
+    static final int MINIMUM_NUM_OF_TREES_TO_SEED = 10;
+
+    /**Maximum number of trees that can be planted when the farm is seeded.*/
+    static final int MAXIMUM_NUM_OF_TREES_TO_SEED = 10000;
+
+    /**Minimum age of a tree planted when the farm is seeded.*/
+    static final int MINIMUM_TREE_AGE = 0;
+
+    /**Maximum age of a tree planted when the farm is seeded.*/
+    static final int MAXIMUM_TREE_AGE = 1000;
+
+    /**Minimum trunk circumference of a tree planted when the farm is seeded.*/
+    static final double MINIMUM_TREE_CIRCUMFERENCE = 10.0;
+
+    /**Maximum trunk circumference of a tree planted when the farm is seeded.*/
+    static final double MAXIMUM_TREE_CIRCUMFERENCE = 100.0;
+
     private ArrayList<Tree> farm = new ArrayList<>();
 
     /**
@@ -29,6 +48,17 @@ public class Plantation {
         } else {
             farm.add(sapling);
         }
+    }
+
+    /**
+     * Populates the farm with trees.
+     * The number of trees planted will be random within the seed bound constants,
+     * and each will have random instance variables.
+     *
+     * @return the number of trees added to the farm.
+     */
+    public int seed() {
+
     }
 
 }
