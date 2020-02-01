0931853721d51aa0d2e437a89b819fe430ca7955
Cragzu <cragzu@gmail.com>
2020-02-01 15:30:06 -0800

    Created constants for Pool

--- src/ca/bcit/comp2522/assignments/a2/Pool.java
+++ src/ca/bcit/comp2522/assignments/a2/Pool.java
@@ -1,8 +1,36 @@
 package ca.bcit.comp2522.assignments.a2;
 
 /**
+ * Creates a Pool object to house Guppies.
+ *
  * @author Chloe Glave
  * @version 2020
  */
 public class Pool {
+    //<editor-fold desc="Constants">
+    /**The default name for a pool.*/
+    static final String DEFAULT_POOL_NAME = "Unnamed";
+
+    /**The default pool temperature in degrees Celsius.*/
+    static final double DEFAULT_POOL_TEMP_CELSIUS = 40.0;
+
+    /**The minimum temperature a pool can be in degrees Celsius.*/
+    static final double MINIMUM_POOL_TEMP_CELSIUS = 0.0;
+
+    /**The maximum temperature a pool can be in degrees Celsius.*/
+    static final double MAXIMUM_POOL_TEMP_CELSIUS = 100.0;
+
+    /**The value of a neutral pH level.*/
+    static final double NEUTRAL_PH = 7.0;
+
+    /**The default nutrient coefficient in a pool.*/
+    static final double DEFAULT_NUTRIENT_COEFFICIENT = 0.50;
+
+    /**The minimum nutrient coefficient a pool can have.*/
+    static final double MINIMUM_NUTRIENT_COEFFICIENT = 0.0;
+
+    /**The maximum nutrient coefficient a pool can have.*/
+    static final double MAXIMUM_NUTRIENT_COEFFICIENT = 1.0;
+    //</editor-fold>
+
 }
