4be8423197d47ab862b3af63940d6fe58563caa8
Cragzu <cragzu@gmail.com>
2020-02-26 21:36:13 -0800

    created constructor and instance var

--- src/ca/bcit/comp2522/assignments/a3/AbstractOperation.java
+++ src/ca/bcit/comp2522/assignments/a3/AbstractOperation.java
@@ -7,4 +7,22 @@ package ca.bcit.comp2522.assignments.a3;
 public abstract class AbstractOperation implements Operation {
      /**The type of operation to be used; add, subtract, multiply, or divide.*/
     protected char operationType;
+
+    /**
+     * Constructor.
+     *
+     * @param operationType char - the type of operation to be performed.
+     */
+    public AbstractOperation(final char operationType) {
+        this.operationType = operationType;
+    }
+
+    /**
+     * Gets the operationType.
+     *
+     * @return char - the operation to be performed.
+     */
+    public final char getOperationType() {
+        return operationType;
+    }
 }
