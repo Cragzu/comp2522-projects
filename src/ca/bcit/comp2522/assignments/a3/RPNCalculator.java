49e17a67aca33a30e1125b800719863c75f65805
Cragzu <cragzu@gmail.com>
2020-02-27 21:29:15 -0800

    Created processFormula method

--- src/ca/bcit/comp2522/assignments/a3/RPNCalculator.java
+++ src/ca/bcit/comp2522/assignments/a3/RPNCalculator.java
@@ -1,11 +1,17 @@
 package ca.bcit.comp2522.assignments.a3;
 
+import java.util.Scanner;
+
 /**
+ * Reverse Polish Notation calculator.
+ *
  * @author Chloe Glave
  * @version 2020
  */
 public class RPNCalculator {
-    /**The minimum possible size a stack can be.*/
+    /**
+     * The minimum possible size a stack can be.
+     */
     static final int MIN_STACK_SIZE = 2;
 
     private Stack stack;
@@ -23,4 +29,24 @@ public class RPNCalculator {
         }
         this.stack = new Stack(stackSize);
     }
+
+
+    /**
+     * Processes formulae using reverse polish notation.
+     *
+     * @param formula String - numbers and operand characters forming the mathematical expression.
+     * @return Operation - the type of operation we are using.
+     * @throws IllegalArgumentException if formula is an invalid string.
+     */
+    public int processFormula(final String formula) throws IllegalArgumentException {
+        if (formula == null || formula.length() == 0) {
+            throw new IllegalArgumentException("That formula doesn't exist!");
+        }
+
+        return 0;
+
+
+
+    }
+
 }
