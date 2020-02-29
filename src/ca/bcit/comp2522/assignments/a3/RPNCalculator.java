1f303df20c8bdad5d0629cd5f890379a00697fb9
Cragzu <cragzu@gmail.com>
2020-02-29 12:45:42 -0800

    Created getResult method, updated processFormula

--- src/ca/bcit/comp2522/assignments/a3/RPNCalculator.java
+++ src/ca/bcit/comp2522/assignments/a3/RPNCalculator.java
@@ -43,7 +43,14 @@ public class RPNCalculator {
         stack.push(operand);
     }
 
-    private Operation getOperation(final char symbol) {
+    /**
+     * Determines the type of operation to use in a calculation.
+     *
+     * @param symbol char - the operation to use.
+     * @return Operation - the appropriate Operation object to use.
+     * @throws InvalidOperationTypeException if the given symbol is not recognized.
+     */
+    private Operation getOperation(final char symbol) throws InvalidOperationTypeException {
         switch (symbol) {
             case AdditionOperation.ADDITION_CODE:
                 return new AdditionOperation();
@@ -54,10 +61,9 @@ public class RPNCalculator {
             case DivisionOperation.DIVISION_CODE:
                 return new DivisionOperation();
             default:
-
+                throw new InvalidOperationTypeException("That symbol wasn't recognized!", symbol);
         }
     }
-
 
     /**
      * Processes formulae using reverse polish notation.
@@ -65,15 +71,51 @@ public class RPNCalculator {
      * @param formula String - numbers and operand characters forming the mathematical expression.
      * @return Operation - the type of operation we are using.
      * @throws IllegalArgumentException if formula is an invalid string.
+     * @throws StackOverflowException if the stack is full and cannot accept a new operand.
      */
-    public int processFormula(final String formula) throws IllegalArgumentException {
+    public int processFormula(final String formula) throws IllegalArgumentException,
+            StackOverflowException, InvalidOperationTypeException {
+
         if (formula == null || formula.length() == 0) {
             throw new IllegalArgumentException("That formula doesn't exist!");
         }
 
+        Scanner scanner = new Scanner(formula);
+        Operation currentOperation;
+
+        while (scanner.hasNext()) {
+            if (scanner.hasNextInt()) { // next item is an operand int
+                this.push(scanner.nextInt());
+            } else { // next item is an operator char
+                currentOperation = this.getOperation(scanner.next().charAt(0));
+            }
+            //pass to perform, invoke getresult
+
+
+        }
         return 0;
+    }
 
+    /**
+     * Retrieves the current value in the stack; the result.
+     *
+     * @return int - the result of some operation.
+     * @throws StackUnderflowException if the stack is empty and nothing can be retrieved.
+     */
+    public int getResult() throws StackUnderflowException {
+        if (stack.size() == 0) {
+            throw new StackUnderflowException("There are no operands!");
+        }
+        return stack.peek(); // the operation result on top of the stack
+    }
 
+    public static void main(String[] args) {
+        String test = "+ - + / * +";
+        Scanner scanner = new Scanner(test);
+
+        while (scanner.hasNext()) {
+            System.out.println(scanner.next().charAt(0));
+        }
 
     }
 
