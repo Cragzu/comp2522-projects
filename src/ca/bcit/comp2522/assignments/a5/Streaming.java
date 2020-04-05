db90f843b4d7ad8ffc7e2c2d324588ba87222b6a
Chloe (Desktop) <cragzu@gmail.com>
2020-04-05 13:07:37 -0700

    Initial commit for lab 10/A5

---
+++ src/ca/bcit/comp2522/assignments/a5/Streaming.java
@@ -0,0 +1,13 @@
+package ca.bcit.comp2522.assignments.a5;
+
+/**
+ * Demonstrates some applications of streaming in Java.
+ *
+ * @author Chloe Glave
+ * @version 2020
+ */
+public class Streaming {
+    public static void main(String[] args) {
+
+    }
+}
