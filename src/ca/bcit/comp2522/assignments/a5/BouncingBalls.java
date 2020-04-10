a0b4fa1fd52094f7689251c16661d8b8a7e3d893
Chloe (Desktop) <cragzu@gmail.com>
2020-04-10 12:26:10 -0700

    Refactored code to use multiple methods for getting input and moved Ball objects into an ArrayList

--- src/ca/bcit/comp2522/assignments/a5/BouncingBalls.java
+++ src/ca/bcit/comp2522/assignments/a5/BouncingBalls.java
@@ -1,12 +1,11 @@
 package ca.bcit.comp2522.assignments.a5;
 
-
-import ca.bcit.comp2522.assignments.a5.Ball;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -19,6 +18,35 @@ import java.util.Scanner;
 public class BouncingBalls extends Application {
 
     /**
+     * Gets user input for the number of Balls to create.
+     *
+     * @return the desired number.
+     */
+    private int getNumberOfBallsFromInput() {
+        final Scanner titleScanner = new Scanner(System.in);
+        System.out.print("How many balls do you want?: ");
+
+        return titleScanner.nextInt();
+    }
+
+    /**
+     * Creates an ArrayList of the specified number of Ball objects.
+     *
+     * @return the created ArrayList.
+     */
+    private ArrayList<Ball> generateListOfBallsFromInput() {
+        ArrayList<Ball> balls = new ArrayList<>();
+        int numOfBalls = getNumberOfBallsFromInput();
+
+        for (int i = 0; i < numOfBalls; i++) {
+            Ball ball = new Ball(250, 250);
+
+            balls.add(ball);
+        }
+        return balls;
+    }
+
+    /**
      * Demonstrates threading in JavaFX.
      * @param primaryStage contains the Scene
      */
@@ -26,25 +54,18 @@ public class BouncingBalls extends Application {
         Pane canvas = new Pane();
         Scene scene = new Scene(canvas, 500, 500);
 
-        final Scanner titleScanner = new Scanner(System.in);
-        System.out.print("How many balls do you want?: ");
+        ArrayList<Ball> balls = generateListOfBallsFromInput();
 
-        final double numOfBalls = titleScanner.nextInt();
-
-        for (int i = 0; i < numOfBalls; i++) {
-            Ball ball = new Ball(250, 250);
-
+        balls.forEach(ball -> {
             canvas.getChildren().add(ball);
-
             Thread bouncer = new Thread(ball);
             bouncer.setDaemon(true);
             bouncer.start();
-        }
+        });
 
         primaryStage.setTitle("Threads and Balls");
         primaryStage.setScene(scene);
         primaryStage.show();
-
     }
 
     /**
